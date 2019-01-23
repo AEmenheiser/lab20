c3b6779aaef906decabc8040bf8212bc8c7726df
unknown <ba7399@gmail.com>
2019-01-23 17:11:00 -0500

    first commit

---
+++ lab20.1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab20._1.Controllers
+{
+
+    /// <summary>
+    /// HEY! sorry, I was lazy with this but I bought a house on friday and had very little time!!! its an excuse, but
+    /// its the only one i got
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
